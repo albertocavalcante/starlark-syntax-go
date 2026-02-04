albertocavalcante/starlark-syntax-go

commit 7daa2a118862f988bfae8a55c7313ca5254f8a0a

Synced-From: bazelbuild/buildtools/build Fixing bug where comment added to multiline attribute was added as suffix (#1434)
- Also implementing tests for cmdComment

Co-authored-by=Tim Malmström <oreflow@google.com>
GitOrigin-RevId: dc2c657ea7a30b0c1fbd00c865d433643bf44a62

bzl/position.go
@@ -79,3 +79,9 @@ func GetTypes(t Expr) []string {
 		return []string{}
 	}
 }
+
+// IsMultiLine returns whether an Expr is multiline or not.
+func IsMultiLine(param Expr) bool {
+	start, end := param.Span()
+	return start.Line != end.Line
+}

commit 6b2546b5085cd57e5dbdc066e3e313526a7883b2

Synced-From: bazelbuild/buildtools/build Update utils.go
Fixes breakage caused by #1434

GitOrigin-RevId: eb7356da689578567b6ba8534b900c20781a835d

bzl/position.go
@@ -83,5 +83,9 @@ func GetTypes(t Expr) []string {
 // IsMultiLine returns whether an Expr is multiline or not.
 func IsMultiLine(param Expr) bool {
 	start, end := param.Span()
+	if end.Line == 0 && end.Byte == 0 && end.LineRune == 0 {
+		// If the end position is unknown, we assume it's single line (for backwards compatibility).
+		return false
+	}
 	return start.Line != end.Line
 }